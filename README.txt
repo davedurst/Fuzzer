883f88a80aedce163447260be9d7be500702117f
David Durst <dnd7249@g.rit.edu>
2016-02-14 00:07:05 -0500

    Added basic command line parsing

---
+++ README.txt
@@ -0,0 +1,4 @@
+Fuzzer
+SWEN 331
+Team 4: David Durst, Kris Brown, Kaicheng Guo
+---------------------------------------------
--- fuzzer.py
+++ fuzzer.py
@@ -6,9 +6,40 @@
 
 import argparse, sys
 
-#This is the entry point for the application
+# This is the entry point for the application
+
 def main():
-    return
+    
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument('--custom-auth', nargs='?', type=str)
+    parent_parser.add_argument('url')
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='Commands', description='The commands the app can run', dest='cmd')
+
+    """ Discover options """
+    discover_parser = subparsers.add_parser('discover', parents=[parent_parser])
+    discover_parser.add_argument('--common-words', nargs='?', type=str, required=True)
+
+    """ Test options """
+    test_parser = subparsers.add_parser('test', parents=[parent_parser])
+    test_parser.add_argument('--common-words', nargs='?', type=str, required=True)
+    test_parser.add_argument('--vectors', nargs='?', type=str, required=False)
+    test_parser.add_argument('--sensitive', nargs='?', type=str, required=False)
+    test_parser.add_argument('--random', action='store_true', default=False)
+    test_parser.add_argument('--slow', nargs='?', type=int, default=500)
+    
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+
+        if args.cmd == 'discover':
+            print("Running discover")
+            print(args)
+        elif args.cmd == 'test':
+            print("Running test")
+            print(args)
 
 if __name__ == '__main__':
     main()
