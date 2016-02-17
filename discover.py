43563413b9e12ba3e51eeb3d886d57f547699d45
David Durst <dnd7249@g.rit.edu>
2016-02-17 09:52:22 -0500

    insert good commit message here

--- discover.py
+++ discover.py
@@ -5,6 +5,36 @@
 """
 
 def discover(args):
-	'''
-		Do discover stuff
-	'''
+	
+	""" Set up custom_auth """
+	"""
+	Psuedo: 
+	
+	if args.custom_auth != None
+		if dvwa
+			visit dvwa with proper credentials
+		if bodgeit
+			visit bodgeit with proper credentials
+		else
+			invalid auth
+	"""
+	
+	print("*** Link Discovery ***")
+	
+	""" Basic crawler (utilize a stack) """
+	
+	print("*** Guessing Links ***")
+	
+	""" append common words with common urls to a base url """
+	
+	print("*** Parsing URLS ***")
+	
+	""" split the urls """
+	
+	print("*** Discovering Form Parameters ***")
+	
+	""" probably some api call """
+	
+	print("*** Discovering Cookies ***")
+	
+	""" probably some api call """
