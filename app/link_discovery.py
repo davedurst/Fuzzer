577b133043a23be3f9c39eb4dea92df86f89f662
David Durst <dnd7249@g.rit.edu>
2016-02-24 16:33:31 -0500

    possible regex for finding urls

--- app/link_discovery.py
+++ app/link_discovery.py
@@ -5,6 +5,8 @@
     within a webpage
 """
 
+import re
+
 def crawl(url, session):
     
     """ initialize url stack """
@@ -20,5 +22,7 @@ def crawl(url, session):
             """ Visit link and find urls within """
             request = session.get(curr_url)
             print("Crawling: " + curr_url)
+            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', curr_url)
+            print(urls)
     
     return discovered_links
