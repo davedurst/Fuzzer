47e8722e8367a2079c33dcd40a3b15678e69a91d
David Durst <dnd7249@g.rit.edu>
2016-02-24 17:07:05 -0500

    finalizing discovery of links

--- app/link_discovery.py
+++ app/link_discovery.py
@@ -16,13 +16,22 @@ def crawl(url, session):
     
     while(len(url_stack) > 0):
         curr_url = url_stack.pop()
+        discovered_links.add(curr_url)
+        
         if "logout" in curr_url:
             """ Dont visit a logout link """
         else:
             """ Visit link and find urls within """
             request = session.get(curr_url)
             print("Crawling: " + curr_url)
-            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', curr_url)
-            print(urls)
+            
+            """ Regular expression to find urls """
+            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', request.text)
+            
+            """ Make sure we dont go surfing the web (rit is for testing) """
+            for url in urls:
+                if "localhost" in url or "127.0.0.1" in url or "www.rit.edu" in url:
+                    url_stack.push()
+                    print("Found: " + url)
     
     return discovered_links
