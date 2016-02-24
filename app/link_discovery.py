e75fa612a89681d601bfc1fe1765eb5cdf2d89e8
David Durst <dnd7249@g.rit.edu>
2016-02-24 12:43:05 -0500

    add skeleton to crawling algorithm

--- app/link_discovery.py
+++ app/link_discovery.py
@@ -6,4 +6,19 @@
 """
 
 def crawl(url, session):
-    print(url)
+    
+    """ initialize url stack """
+    url_stack = set()
+    url_stack.add(url)
+    discovered_links = set()
+    
+    while(len(url_stack) > 0):
+        curr_url = url_stack.pop()
+        if "logout" in curr_url:
+            """ Dont visit a logout link """
+        else:
+            """ Visit link and find urls within """
+            request = session.get(curr_url)
+            print("Crawling: " + curr_url)
+    
+    return discovered_links
