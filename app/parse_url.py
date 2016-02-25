1e811a87920223e3fc77fa203621134b1e7c2f33
David Durst <dnd7249@g.rit.edu>
2016-02-25 09:58:23 -0500

    parse shietttt

--- app/parse_url.py
+++ app/parse_url.py
@@ -15,13 +15,14 @@ def parse(urls):
         if(len(sub_divide) > 1):
             """ get param key/value pairs """
             url_parameters = sub_divide[1].split('&')
-            
+
             for key_val in url_parameters:
-                key, val = key_val.split('=')
-                print("Parameter: " + key + "; Value: " + val)
+                if(len(key_val.split('=')) == 2):
+                    key, val = key_val.split('=')
+                    print("Parameter: " + key + "; Value: " + val)
         else:
             print("No parameters found")
             
 # test sequence
 parse(["https://github.com/davedurst/Fuzzer/edit/master/app/parse_url.py"])
-pares(["https://support.google.com/webmasters/answer/6080550?hl=en", "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=print%20all%20cookies%20for%20a%20session%20requests%20python"]
+parse(["https://support.google.com/webmasters/answer/6080550?hl=en", "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=print%20all%20cookies%20for%20a%20session%20requests%20python"])
