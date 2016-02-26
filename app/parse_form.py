ad470dcd8ebdc40ef890bc1154f6f15b49d11d50
David Durst <dnd7249@g.rit.edu>
2016-02-25 23:56:49 -0500

    parse forms about done

--- app/parse_form.py
+++ app/parse_form.py
@@ -4,11 +4,32 @@
     Group 4
 """
 
+import utils.bs4 as bSoup
+
 def parse(urls, session):
-    #formz
+    
+    for url in urls:
+        print("Parsing form inputs for url: " + url)
+        
+        req = session.get(url)
+        parse = bSoup(req.text)
+        forms = parse.findAll('form')
+        
+        if !len(forms):
+            print("No forms found on page")
+        
+        for form in forms:
+            print("Form name:   " + form["name"])
+            print("Form method: " + form["name"])
+            
+            for tag in form.recursiveChildGenerator():
+                if(tag.name == "input"):
+                    if("name" in tag.attrs and "value" in tag.attrs):
+                        print("Input tag name: " + tag["name"])
+                        print("Input tag value: " + tag["value"])
 
 def main():
-    #test
+    parse(["https://mycourses.rit.edu/"])
 
 if __name__ == "__main__":
     main()
