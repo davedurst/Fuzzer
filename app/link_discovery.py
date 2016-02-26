aacc1a12beb174de1be41f7adf0628f60fde718e
David Durst <dnd7249@g.rit.edu>
2016-02-26 08:56:48 -0500

    changed strategy bc of partial links ie href="."

--- app/link_discovery.py
+++ app/link_discovery.py
@@ -7,6 +7,8 @@
     within a webpage
 """
 
+from app.packages.bs4 import BeautifulSoup, SoupStrainer
+from urllib.parse import urljoin
 import re
 
 def crawl(url, session):
@@ -25,17 +27,29 @@ def crawl(url, session):
         else:
             """ Visit link and find urls within """
             request = session.get(curr_url)
-            print("Crawling: " + curr_url)
+            print("\nCrawling: " + curr_url)
+ 
+            """ make partial link full """
+            soup = BeautifulSoup(request.text)
+            all_links = soup.findAll('a')
+            for link in all_links:
+                curr_link = urljoin(curr_url, link.get('href'))
+
+                if curr_link not in discovered_links:
+                    if "localhost" in curr_link or "127.0.0.1" in curr_link or "www.rit.edu" in curr_link:
+                        url_stack.add(curr_link)
+                        discovered_links.add(curr_link)
+                        print("Discovered: " + curr_link)
+						
+            # """ Regular expression to find urls """
+            # urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', request.text)
             
-            """ Regular expression to find urls """
-            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', request.text)
-            
-            """ Make sure we dont go surfing the web (rit is for testing) """
-            for link in urls:
-                if link not in discovered_links:
-                    if "localhost" in link or "127.0.0.1" in link or "www.rit.edu" in link:
-                        url_stack.add(link)
-                        discovered_links.add(link)
-                        print("Found: " + link)
+            # """ Make sure we dont go surfing the web (rit is for testing) """
+            # for link in urls:
+                # if link not in discovered_links:
+                    # if "localhost" in link or "127.0.0.1" in link or "www.rit.edu" in link:
+                        # url_stack.add(link)
+                        # discovered_links.add(link)
+                        # print("Found: " + link)
     
     return discovered_links
