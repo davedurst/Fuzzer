a0a4dbbffe2727c5c7b6eea9e54382afe995017d
David Durst <dnd7249@g.rit.edu>
2016-03-11 21:41:52 -0500

    Update test.py

--- app/test.py
+++ app/test.py
@@ -5,19 +5,19 @@
 """
 
 def test(param_dict, form_dict, args):
-  """ access the global session """
+  # access the global session
   global sess
-  
-  #read vectors
-  
-  #attack forms
-  
-  #attack url params
+  # read vectors
+  vectors = open(args.vectors).readlines()
+  # attack forms
+  test_forms(form_dict, vectors, sess, args.random, args.timeout)
+  # attack url params
+  test_url_param(param_dict, vectors, sess, args.random, args.timeout)
 
-def test_forms(form_dict, vectors, session, args.random, args.timeout):
+def test_forms(form_dict, vectors, session, rand, timeout):
     #attack all inputs with vectors
     return
   
-def test_url_param(param_dict, vectors, session, args.random, args.timeout):
+def test_url_param(param_dict, vectors, session, rand, timeout):
     #attack all url params with vectors
     return
