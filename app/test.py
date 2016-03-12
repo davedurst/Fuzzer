8c050432aa7b39341140c36f7cd952144c104b05
David Durst <dnd7249@g.rit.edu>
2016-03-11 23:24:42 -0500

    Implemented timeout analysis

--- app/test.py
+++ app/test.py
@@ -23,8 +23,13 @@ def get_status_code(code):
     else:
         return code.__str__() + " --> Unknown Code" 
 
-def analyze_response_time(response):
-
+#Detects responses that take longer than expected
+def analyze_response_time(response, timeout):
+    load_time = response.elapsed.total_seconds() * 1000
+    if load_time > timeout:
+        print("Response took " + load_time + " ms.")
+        print("Potential Denial Of Service Vulnerability")
+            
 #Detects unusual response codes and reports them
 def analyze_status_code(response):
     if response.status_code != 200:
@@ -37,14 +42,14 @@ def analyze_sanitization(response, vector):
     return
 
 def test(param_dict, form_dict, args):
-  # access the global session
-  global sess
-  # read vectors
-  vectors = open(args.vectors).readlines()
-  # attack forms
-  test_forms(form_dict, vectors, sess, args.random, args.timeout)
-  # attack url params
-  test_url_param(param_dict, vectors, sess, args.random, args.timeout)
+    # access the global session
+    global sess
+    # read vectors
+    vectors = open(args.vectors).readlines()
+    # attack forms
+    test_forms(form_dict, vectors, sess, args.random, args.timeout)
+    # attack url params
+    test_url_param(param_dict, vectors, sess, args.random, args.timeout)
 
 def test_forms(form_dict, vectors, session, rand, timeout):
     #attack all inputs with vectors
@@ -52,4 +57,4 @@ def test_forms(form_dict, vectors, session, rand, timeout):
   
 def test_url_param(param_dict, vectors, session, rand, timeout):
     #attack all url params with vectors
-    return
+   return
