988b3322d816f2b84b6774dade8e81d54695934e
David Durst <dnd7249@g.rit.edu>
2016-03-11 19:00:35 -0500

    Stubbed the test file

--- app/test.py
+++ app/test.py
@@ -1,1 +1,23 @@
-"""  The eventual test file (part 2) """
+"""
+    Fuzzer Project
+    SWEN-331
+    Group 4
+"""
+
+def test(param_dict, form_dict, args):
+  """ access the global session """
+  global sess
+  
+  #read vectors
+  
+  #attack forms
+  
+  #attack url params
+
+def test_forms(form_dict, vectors, session, args.random, args.timeout):
+    #attack all inputs with vectors
+    return
+  
+def test_url_param(param_dict, vectors, session, args.random, args.timeout):
+    #attack all url params with vectors
+    return
