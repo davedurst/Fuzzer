e9b449331a7e34b02c09a887eb9fe373e006cd35
David Durst <dnd7249@g.rit.edu>
2016-03-11 19:35:03 -0500

    Restructured parameter parsing to return data

--- app/parse_url.py
+++ app/parse_url.py
@@ -5,28 +5,42 @@
 """
 
 def parse(urls):
+    #dictionary of dictionaries (Key: url; Value: Parameter dictionary)
+    param_dict = dict()
     
     """ for each url see if we have parameters """
     for curr_url in urls:
         print("\nFinding parameters for url: " + curr_url)
-        sub_divide = curr_url.split('?')
+        base, curr_url_params = parse_url(curr_url)
+        param_dict[base] = curr_url_params
+
+    return param_dict
+   
+def parse_url(url):
+    url_params = dict()
+    sub_divide = curr_url.split('?')
         
-        """ if we have url parameters """
-        if(len(sub_divide) > 1):
-            """ get param key/value pairs """
-            if '#' in sub_divide[1]:
-                parameters = sub_divide[1].split('#')
-                url_parameters = parameters[0].split('&')
-            else:
-                url_parameters = sub_divide[1].split('&')
-
-            for key_val in url_parameters:
-                if(len(key_val.split('=')) == 2):
-                    key, val = key_val.split('=')
-                    print("Parameter: " + key + "; Value: " + val)
+    """ if we have url parameters """
+    if(len(sub_divide) > 1):
+        """ get param key/value pairs """
+        if '#' in sub_divide[1]: 
+            #case where there are additional url fragments after params
+            parameters = sub_divide[1].split('#')
+            url_parameters = parameters[0].split('&')
         else:
-            print("No parameters found")
+            url_parameters = sub_divide[1].split('&')
 
+        for key_val in url_parameters:
+            if(len(key_val.split('=')) == 2):
+                key, val = key_val.split('=')
+                url_params[key] = val
+                print("Parameter: " + key + "; Value: " + val)
+    else:
+        print("No parameters found")
+    
+    #return the base url and the parameter dictionary
+    return sub_divide[0], url_params
+    
 #some tests
 def main():
     """ Try url with no query parameters """
